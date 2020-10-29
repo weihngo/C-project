2e781833cd79316586bdb168b894758d6f083574
lwh <lwh2017@mail.ustc.edu.cn>
2020-10-29 12:59:36 +0800

    Initial commit

---
+++ text02/cmake-build-debug/CMakeFiles/text02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text02.dir/main.cpp.obj"
+  "libtext02.dll.a"
+  "text02.exe"
+  "text02.exe.manifest"
+  "text02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/text02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ text02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    
+
+
+
+
+    return 0;
+
+}
