d3f70c5e9d67cd24bb4b362b38ec1cfa40ceb542
weihngo <55616781+weihngo@users.noreply.github.com>
2020-10-29 10:55:20 +0800

    Initial commit

---
+++ worker_system/cmake-build-debug/CMakeFiles/worker_system.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/lwh/Desktop/github/C-project/worker_system/main.cpp" "C:/Users/lwh/Desktop/github/C-project/worker_system/cmake-build-debug/CMakeFiles/worker_system.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ worker_system/cmake-build-debug/CMakeFiles/worker_system.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/worker_system.dir/main.cpp.obj"
+  "libworker_system.dll.a"
+  "worker_system.exe"
+  "worker_system.exe.manifest"
+  "worker_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/worker_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
