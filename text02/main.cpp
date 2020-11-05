3984c437b063003589c178a933f16def7e91e41d
lwh <lwh2017@mail.ustc.edu.cn>
2020-11-05 09:56:15 +0800

    Initial commit

--- text02/main.cpp
+++ text02/main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <algorithm>
-#include <functional>
-#include <vector>
 
 using namespace std;
 
 int main() {
-    
-
-
-
-
+    for(int i=0; i < 10; i++){
+        cout << i << endl;
+    }
     return 0;
 
 }
